2746df3fcbe5b0fb793d67ee5ecffa56fc62fd5b
sbx_user1051 <sbx_user1051@169.254.221.253>
2022-09-04 11:18:23 +0000

    Sun Sep 04 11:18:23 UTC 2022

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -4,18 +4,37 @@ public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
 
-
-        if ((number % 5 == 0 || number.toString().contains("5")) && (number % 3 == 0 || number.toString().contains("3"))) {
+        if ((number % 5 == 0 || number.toString().contains("5")) && (number % 3 == 0 || number.toString().contains("3"))
+                && (number < 10 && checkDigits(number))) {
+            return "fizz buzz deluxe";
+        } else if ((number % 5 == 0 || number.toString().contains("5")) && (number % 3 == 0 || number.toString().contains("3"))) {
             return "fizz buzz";
         } else if (number % 5 == 0 || number.toString().contains("5")) {
             return "buzz";
         } else if (number % 3 == 0 || number.toString().contains("3")) {
             return "fizz";
+        } else if (number < 10 && checkDigits(number)) {
+            return "deluxe";
         } else {
             return number.toString();
         }
 
+
     }
 
+    public boolean checkDigits(int nr) {
+        int digit = nr % 10;
+
+        while (nr != 0) {
+            int currentDigit = nr % 10;
+            nr = nr / 10;
+
+            if (digit != currentDigit) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
 
